f62f2e508e8db83e9ee75d9b13bd953cde35d93b
themodernmonk7 <sauravavishek@gmail.com>
2025-02-23 11:53:06 +0530

    fix the eslint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,26 @@
-module.exports = {
-  "env": {
-    "browser": true,
-    "es6": true,
-    "jest/globals": true
+export const env = {
+  browser: true,
+  es6: true,
+  "jest/globals": true,
+}
+export const ext = ["eslint:recommended", "plugin:react/recommended"]
+export const parserOptions = {
+  ecmaFeatures: {
+    jsx: true,
   },
-  "extends": [
-    "eslint:recommended",
-    "plugin:react/recommended"
-  ],
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
-    },
-    "ecmaVersion": 2018,
-    "sourceType": "module"
-  },
-  "plugins": [
-    "react", "jest"
-  ],
-  "rules": {
-    "indent": [
-      "error",
-        2
-      ],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
-    "quotes": [
-      "error",
-      "single"
-    ],
-    "semi": [
-      "error",
-      "never"
-    ],
-    "eqeqeq": "error",
-    "no-trailing-spaces": "error",
-    "object-curly-spacing": [
-      "error", "always"
-    ],
-    "arrow-spacing": [
-      "error", { "before": true, "after": true }
-    ],
-    "no-console": "error",
-    "react/prop-types": 0
-  }
+  ecmaVersion: 2018,
+  sourceType: "module",
+}
+export const plugins = ["react", "jest"]
+export const rules = {
+  indent: ["error", 2],
+  // "linebreak-style": ["error", "unix"],
+  quotes: ["error", "double"],
+  semi: ["error", "never"],
+  eqeqeq: "error",
+  "no-trailing-spaces": "error",
+  "object-curly-spacing": ["error", "always"],
+  "arrow-spacing": ["error", { before: true, after: true }],
+  "no-console": "error",
+  "react/prop-types": 0,
 }
